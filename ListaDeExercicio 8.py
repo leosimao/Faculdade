8e32bc13458090e0e8939fa2d639a6e25195a4b1
leosimao <leonardosc.me@gmail.com>
2022-06-30 07:46:47 -0300

    Lista8: Iniciando com comando Whilhe (ex1)

---
+++ ListaDeExercicio 8.py
@@ -0,0 +1,73 @@
+#Apresente na tela os números de 1 a 100.
+
+# cont = 1
+# while(cont <= 100):
+#     print(cont)
+#     cont += 1
+
+#Faça a soma dos números de 1 a 100 e apresente somente o resultado.
+
+# soma = 0
+# cont = 0
+# while(cont < 100):
+#     soma += cont
+#     cont += 1
+# print(soma)
+
+
+#Apresente na tela somente os números pares entre 50 e 100
+
+# cont = 50
+# while(cont <= 100):
+#     if(cont % 2 == 0):
+#         print(cont)
+#     cont += 1
+
+#Apresente na tela somente os números ímpares entre 1 e 50.
+
+# cont = 1
+# while(cont <= 50):
+#     if(cont % 2 == 1):
+#         print(cont)
+#     cont += 1
+
+#Apresente na tela somente a soma dos números pares entre 1 e 100.
+
+# cont = 1
+# soma = 0
+# while(cont <= 100):
+#     if(cont % 2 == 0):
+#         soma += cont
+#         print(soma)
+#     cont += 1
+# print(soma)
+        
+#Apresente na tela os números de X a Y (peça para o usuário informar os valores de X e de Y).
+
+# x = int(input("Digite o primeiro valor: "))
+# y = int(input("Digite o segundo valor: "))
+
+# while(x <= y):
+#     print(x)
+#     x += 1
+
+#Faça a soma dos números de X a Y e apresente somente o resultado (peça para o usuário informar os valores de X e de Y).
+
+# x = int(input("Digite o primeiro valor: "))
+# y = int(input("Digite o segundo valor: "))
+
+# soma = 0
+# while(x <= y):
+#     soma += x
+#     x += 1
+# print(soma)
+
+#Apresente na tela somente os números ímpares entre X e Y (peça para o usuário informar os valores de X e de Y).
+
+# x = int(input("Digite o primeiro valor: "))
+# y = int(input("Digite o segundo valor: "))
+
+# while(x <= y):
+#     if(x % 2 == 1):
+#         print(x)
+#     x += 1
